0002a4fd77b29e9f6e1272955f9d0ce9879219f5
Serdar Büyüktemiz <hserdarb@gmail.com>
2012-12-26 10:58:43 +0200

    deleted on migration added

--- Client/RestPOC.Web/Controllers/HomeController.cs
+++ Client/RestPOC.Web/Controllers/HomeController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace RestPOC.Web.Controllers
+﻿namespace RestPOC.Web.Controllers
 {
+    using System.Web.Mvc;
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Threading.Tasks;
